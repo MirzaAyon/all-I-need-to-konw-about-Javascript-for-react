2da1d70d9f89f14c867aaa7009bfdbe8d9e1e397
MirzaAyon <ayonmirza98@gmail.com>
2022-07-25 22:00:18 +0600

    some practice and quiz added

---
+++ quiz.js
@@ -0,0 +1,28 @@
+Which one is true about map and forEach when called on an empty array ?
+//map returns array but foreach returns undefined
+
+const adventurer = { name: 'Alice', cat: { name: 'Lucy' } };
+const ratName = adventurer?.rat?.name;
+//undefined ans
+
+let person = null;
+console.log(person ? person : "person is null");
+What will be the output of the above code ?
+    //person is null  - ans
+
+
+    Which one is not true for the filter(array method filter) ?
+//bhul returns undefined when no elements pass the test - false
+//bhul returns empty array when no elements pass the test - true
+
+
+const obj = { a: 1 };
+console.log(Object.keys(obj).length === 0);
+What will be the output ?
+    //false ans
+
+
+    which one is false
+//we can check length of string and array
+//reducer is an array method
+//{} "empty object" is falsy value
